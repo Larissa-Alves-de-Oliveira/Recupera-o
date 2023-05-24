ab38515c854038cc101c0a59d9104a7d239a0335
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-05-24 11:52:22 -0300

    rec

---
+++ ex1.php
@@ -0,0 +1,8 @@
+<?php 
+    $a = 10;
+    $b = 5;
+    echo "O valor de A é: $a\n";
+    echo "O valor de B é: $b\n";
+    echo "O valor de A é: $a\nO valor da variável B é: $b\n";
+    echo "Valor de A = ".$a."\n"."Valor de B = ".$b;
+?>
