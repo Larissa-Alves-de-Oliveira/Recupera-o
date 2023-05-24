ee4183e15be5d6f1405e19dc95d5e04e800f05f9
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-05-24 12:12:11 -0300

    first commit

---
+++ ex2.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 10;
+ $b = 5;
+	
+ $soma = $a + $b;
+ $subtracao = $a - $b;
+ $Divisao = $a / $b;
+ $multiplicacao = $a * $b;
+	
+ echo "Soma: $soma\n";
+ echo "Subtração: $subtracao\n";
+ echo "Divisão: $Divisao\n";
+ echo "Multiplicação: $multiplicacao\n";
+ echo "Soma de a e b = ".$soma."\n";
+
+?>
